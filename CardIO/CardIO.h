7ef8f32ce99ef099bedb5ab898175240760a95bc
Kra Larivain <olarivain@gmail.com>
2015-06-09 17:15:13 -0700

    added project for Carthage

--- CardIO/CardIO.h
+++ CardIO/CardIO.h
@@ -3,7 +3,19 @@
 //  Version 5.0.6
 //
 //  See the file "LICENSE.md" for the full license governing this code.
-//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cardIO.
+FOUNDATION_EXPORT double cardIOVersionNumber;
+
+//! Project version string for cardIO.
+FOUNDATION_EXPORT const unsigned char cardIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cardIO/PublicHeader.h>
+
+
+
 
 // All-in-one header file for card.io sdk.
 #import "CardIOCreditCardInfo.h"
